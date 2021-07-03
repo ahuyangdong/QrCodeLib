396dc11c254c269893d49faa55d0ce4a486a3be7
余静 <yu1441@qq.com>
2021-07-03 11:08:16 +0800

    添加打包方法

--- README.md
+++ README.md
@@ -30,3 +30,99 @@ https://blog.csdn.net/ahuyangdong/article/details/89339970
 https://www.jianshu.com/p/e80a85b17920
 
 https://github.com/HappyMiao/QrCodeScan
+
+
+# zxing-lib 二维码使用
+
+### 引入
+#### 主gradle中添加
+```gradle
+buildscript {
+    repositories {
+        maven { url 'http://kotlinx.com/repository' }
+    }
+}
+```
+#### 项目工程gradle中添加
+```gradle
+implementation 'com.kotlinx.zxing-lib:zxing-lib:1.0.0'
+```
+
+
+
+### 扫描二维码
+```java
+// 开始扫码
+private void startQrCode() {
+    // 申请相机权限
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+        // 申请权限
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                .CAMERA)) {
+            Toast.makeText(this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_SHORT).show();
+        }
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, Constant.REQ_PERM_CAMERA);
+        return;
+    }
+    // 申请文件读写权限（部分朋友遇到相册选图需要读写权限的情况，这里一并写一下）
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        // 申请权限
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                .WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_SHORT).show();
+        }
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Constant.REQ_PERM_EXTERNAL_STORAGE);
+        return;
+    }
+    // 二维码扫码
+    Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+    startActivityForResult(intent, Constant.REQ_QR_CODE);
+}
+
+
+    @Override
+public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    switch (requestCode) {
+        case Constant.REQ_PERM_CAMERA:
+            // 摄像头权限申请
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 获得授权
+                startQrCode();
+            } else {
+                // 被禁止授权
+                Toast.makeText(MainActivity.this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_LONG).show();
+            }
+            break;
+        case Constant.REQ_PERM_EXTERNAL_STORAGE:
+            // 文件读写权限申请
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 获得授权
+                startQrCode();
+            } else {
+                // 被禁止授权
+                Toast.makeText(MainActivity.this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_LONG).show();
+            }
+            break;
+    }
+}
+
+
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    //扫描结果回调
+    if (requestCode == Constant.REQ_QR_CODE && resultCode == RESULT_OK) {
+        Bundle bundle = data.getExtras();
+        String scanResult = bundle.getString(Constant.INTENT_EXTRA_KEY_QR_SCAN);
+        //将扫描出的信息显示出来
+        tvResult.setText(scanResult);
+    }
+}
+```
+
+
+### 生成二维码
+```java
+Bitmap bitmap = QrCodeGenerator.getQrCodeImage("yujing", 512, 512);
+```
--- app/src/main/java/com/dommy/qrcodelib/MainActivity.java
+++ app/src/main/java/com/dommy/qrcodelib/MainActivity.java
@@ -45,31 +45,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imgQrcode = findViewById(R.id.img_qrcode);
     }
 
-    // 开始扫码
-    private void startQrCode() {
-        // 申请相机权限
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            // 申请权限
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
-                    .CAMERA)) {
-                Toast.makeText(this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_SHORT).show();
-            }
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, Constant.REQ_PERM_CAMERA);
-            return;
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_qrcode:
+                startQrCode();
+                break;
+            case R.id.btn_generate:
+                generateQrCode();
+                break;
+            default:
+                break;
         }
-        // 申请文件读写权限（部分朋友遇到相册选图需要读写权限的情况，这里一并写一下）
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // 申请权限
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
-                    .WRITE_EXTERNAL_STORAGE)) {
-                Toast.makeText(this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_SHORT).show();
-            }
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Constant.REQ_PERM_EXTERNAL_STORAGE);
-            return;
-        }
-        // 二维码扫码
-        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
-        startActivityForResult(intent, Constant.REQ_QR_CODE);
     }
 
     /**
@@ -89,57 +76,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
+
+// 开始扫码
+private void startQrCode() {
+    // 申请相机权限
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+        // 申请权限
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                .CAMERA)) {
+            Toast.makeText(this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_SHORT).show();
+        }
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, Constant.REQ_PERM_CAMERA);
+        return;
+    }
+    // 申请文件读写权限（部分朋友遇到相册选图需要读写权限的情况，这里一并写一下）
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        // 申请权限
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                .WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_SHORT).show();
+        }
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Constant.REQ_PERM_EXTERNAL_STORAGE);
+        return;
+    }
+    // 二维码扫码
+    Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+    startActivityForResult(intent, Constant.REQ_QR_CODE);
+}
+
+
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_qrcode:
+public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    switch (requestCode) {
+        case Constant.REQ_PERM_CAMERA:
+            // 摄像头权限申请
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 获得授权
                 startQrCode();
-                break;
-            case R.id.btn_generate:
-                generateQrCode();
-                break;
-            default:
-                break;
-        }
+            } else {
+                // 被禁止授权
+                Toast.makeText(MainActivity.this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_LONG).show();
+            }
+            break;
+        case Constant.REQ_PERM_EXTERNAL_STORAGE:
+            // 文件读写权限申请
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 获得授权
+                startQrCode();
+            } else {
+                // 被禁止授权
+                Toast.makeText(MainActivity.this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_LONG).show();
+            }
+            break;
     }
+}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //扫描结果回调
-        if (requestCode == Constant.REQ_QR_CODE && resultCode == RESULT_OK) {
-            Bundle bundle = data.getExtras();
-            String scanResult = bundle.getString(Constant.INTENT_EXTRA_KEY_QR_SCAN);
-            //将扫描出的信息显示出来
-            tvResult.setText(scanResult);
-        }
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case Constant.REQ_PERM_CAMERA:
-                // 摄像头权限申请
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 获得授权
-                    startQrCode();
-                } else {
-                    // 被禁止授权
-                    Toast.makeText(MainActivity.this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_LONG).show();
-                }
-                break;
-            case Constant.REQ_PERM_EXTERNAL_STORAGE:
-                // 文件读写权限申请
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 获得授权
-                    startQrCode();
-                } else {
-                    // 被禁止授权
-                    Toast.makeText(MainActivity.this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_LONG).show();
-                }
-                break;
-        }
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    //扫描结果回调
+    if (requestCode == Constant.REQ_QR_CODE && resultCode == RESULT_OK) {
+        Bundle bundle = data.getExtras();
+        String scanResult = bundle.getString(Constant.INTENT_EXTRA_KEY_QR_SCAN);
+        //将扫描出的信息显示出来
+        tvResult.setText(scanResult);
     }
+}
 
 }
